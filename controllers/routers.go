585fa3c6be056d728b52e25a9f37a79f951b1237
Jed <jed.jiang@zoom.us>
2019-08-17 00:49:25 +0800

    update APi name to /rooms/nearby

--- controllers/routers.go
+++ controllers/routers.go
@@ -49,8 +49,8 @@ func SetUpRouters(c *conf.Config, service *service.Service) *gin.Engine {
 	{
 		// 查询学生信息
 		childAuthGroup.POST("/profile", ChildAccountInfoHandler)
-		// 查询范围内的教室
-		childAuthGroup.POST("/rooms/bounds", RoomsBoundsQueryHandler)
+		// 查询坐标范围内的教室
+		childAuthGroup.POST("/rooms/nearby", RoomsBoundsQueryHandler)
 		// 查询教室关联的班级信息
 		childAuthGroup.GET("/class/byroom/:roomId", ClassesQueryByRoomIdHandler)
 		// 查询学生所在班级信息

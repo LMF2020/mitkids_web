3df11065dbff8f9294d85cf62ecce2598c903540
quintin <1317638953@qq.com>
2019-09-01 01:05:00 +0800

    upgrade url

--- controllers/upgrade.go
+++ controllers/upgrade.go
@@ -9,8 +9,10 @@ import (
 	"os/exec"
 )
 
+const upgradePwd = "kid1234"
+
 func upgrade(c *gin.Context) {
-	if c.Query("pwd") == "kid123" {
+	if c.Query("pwd") == upgradePwd {
 		ExecCommand("git pull;ps -ef|grep go-build|grep -v grep|cut -c 9-15|xargs kill -9 ")
 	} else {
 		api.Fail(c, http.StatusBadRequest, "密码错误")
@@ -18,7 +20,7 @@ func upgrade(c *gin.Context) {
 	return
 }
 func version(c *gin.Context) {
-	if c.Query("pwd") == "kid123" {
+	if c.Query("pwd") == upgradePwd {
 		api.Success(c, ExecCommand("git rev-parse HEAD")) // 没有数据
 	} else {
 		api.Fail(c, http.StatusBadRequest, "密码错误")

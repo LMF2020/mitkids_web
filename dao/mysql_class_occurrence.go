d4db55ed56f8e6d1387c37a5ebad67cbaaf846d6
Jed <jed.jiang@zoom.us>
2019-09-16 23:46:11 +0800

    fix bug: fix columns missing issue on ListScheduledOccurringClass(mysql)

--- dao/mysql_class_occurrence.go
+++ dao/mysql_class_occurrence.go
@@ -15,8 +15,8 @@ func (d *Dao) ListScheduledOccurringClass(classId, scheduledTimeOrder string, oc
 
 	sql := `SELECT 
 			  coo.class_id,
-			  coo.teacher_id,
-			  coo.fore_teacher_id,
+			  c.teacher_id,
+			  c.fore_teacher_id,
 			  c.book_level,
 			  at_1.account_name AS teacher_name,
 			  at_2.account_name AS fore_teacher_name,
@@ -24,20 +24,20 @@ func (d *Dao) ListScheduledOccurringClass(classId, scheduledTimeOrder string, oc
 			  coo.book_code,
 			  bk.book_name,
 			  bk.book_link,
-			  coo.occurrence_status AS status,
-			  coo.schedule_time 
+			  coo.occurrence_status AS STATUS,
+			  coo.occurrence_time
 			FROM
 			  mk_class_occurrence coo 
 			  LEFT JOIN mk_class c 
 				ON coo.class_id = c.class_id 
 			  LEFT JOIN mk_room rm 
-				ON rm.room_id = coo.room_id 
+				ON rm.room_id = c.room_id 
 			  LEFT JOIN mk_book bk 
 				ON bk.book_code = coo.book_code 
 			  LEFT JOIN mk_account at_1 
-				ON at_1.account_id = coo.teacher_id 
+				ON at_1.account_id = c.teacher_id 
 			  LEFT JOIN mk_account at_2 
-				ON at_2.account_id = coo.fore_teacher_id 
+				ON at_2.account_id = c.fore_teacher_id 
 			WHERE c.class_id = ? 
 			  AND coo.occurrence_status = ?
 			ORDER BY coo.schedule_time %s

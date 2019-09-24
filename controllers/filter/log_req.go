1b8b3bd3f3a3fed3f1d24edb09526f344ee1fc12
quintin.zhang <quintin.zhang@zoom.us>
2019-09-24 13:55:12 +0800

    update file upload

--- controllers/filter/log_req.go
+++ controllers/filter/log_req.go
@@ -10,7 +10,12 @@ import (
 func RequestLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		data, _ := ioutil.ReadAll(c.Request.Body)
-		log.Logger.Debug("url:" + c.Request.RequestURI + ",request params:" + string(data))
+		if len(data) > 1000 {
+			log.Logger.Debug("url:" + c.Request.RequestURI + ",request params: too long")
+		} else {
+			log.Logger.Debug("url:" + c.Request.RequestURI + ",request params:" + string(data))
+		}
+
 		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
 		c.Next()
 	}

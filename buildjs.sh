c4eb5c51043f1f154c7b949d03e2320e211d45d0
quintin <1317638953@qq.com>
2019-10-04 11:46:55 +0800

    update upgrade

--- buildjs.sh
+++ buildjs.sh
@@ -2,20 +2,20 @@
 cd /opt/nginxdocker/mulkids-cms-pro
 yarnbash="yarn run build"
 if [ $1 == "all" ]; then
-  yarnbash="yarn &&yarn run build"
+  yarnbash="yarn && yarn run build"
 fi
 git pull
-cd muitkid-cms && ${yarnbash}
+cd muitkid-cms && eval ${yarnbash}
 rm -Rf /opt/nginxdocker/muitkid-web-app/mkcms
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-cms/dist /opt/nginxdocker/muitkid-web-app/mkcms
 
 cd ..
-cd muitkid-stu/ && ${yarnbash}
+cd muitkid-stu/ && eval ${yarnbash}
 rm -Rf /opt/nginxdocker/muitkid-web-app/mkstu
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-stu/dist /opt/nginxdocker/muitkid-web-app/mkstu
 
 cd ..
-cd muitkid-tea/ && ${yarnbash}
+cd muitkid-tea/ && eval ${yarnbash}
 rm -Rf /opt/nginxdocker/muitkid-web-app/mktea
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-tea/dist /opt/nginxdocker/muitkid-web-app/mktea
 

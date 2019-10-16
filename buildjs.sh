26ba9a11963a3a762dc08ca330252325d09742b8
quintin.zhang <quintin.zhang@zoom.us>
2019-10-16 17:34:52 +0800

    update buildjs.sh

--- buildjs.sh
+++ buildjs.sh
@@ -5,21 +5,18 @@ if [ "$1" == "all" ]; then
   yarnbash="yarn && yarn run build"
 fi
 git pull
+rm -Rf /opt/nginxdocker/muitkid-web-app/*
 cd muitkid-cms && eval ${yarnbash}
-rm -Rf /opt/nginxdocker/muitkid-web-app/mkcms
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-cms/dist /opt/nginxdocker/muitkid-web-app/mkcms
 
 cd ..
 cd muitkid-stu/ && eval ${yarnbash}
-rm -Rf /opt/nginxdocker/muitkid-web-app/mkstu
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-stu/dist /opt/nginxdocker/muitkid-web-app/mkstu
 
 cd ..
 cd muitkid-tea/ && eval ${yarnbash}
-rm -Rf /opt/nginxdocker/muitkid-web-app/mktea
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-tea/dist /opt/nginxdocker/muitkid-web-app/mktea
 
 cd ..
 cd muitkid-portal/ && yarn run build
-rm -Rf /opt/nginxdocker/muitkid-web-app/muitkid-portal
 cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-portal/dist /opt/nginxdocker/muitkid-web-app/muitkid-portal

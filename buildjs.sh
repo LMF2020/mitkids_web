98270d038d5daa1fbef7567eca444303b7b4e1ad
quintin.zhang <quintin.zhang@zoom.us>
2019-10-16 14:25:13 +0800

    update buildjs.sh

--- buildjs.sh
+++ buildjs.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 cd /opt/nginxdocker/mulkids-cms-pro
+yarnbash="yarn run build"
+if [ $1 == "all" ]; then
+  yarnbash="yarn && yarn run build"
+fi
 git pull
-cd muitkid-cms && yarn run build 
-rm -Rf /opt/nginxdocker/muitkid-web-app/mkcms
-cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-cms/dist /opt/nginxdocker/muitkid-web-app/mkcms
 
+cd muitkid-cms  && eval ${yarnbash}
+#rm -Rf /opt/nginxdocker/muitkid-web-app/mkcms
+#cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-cms/dist /opt/nginxdocker/muitkid-web-app/mkcms
 cd ..
-cd muitkid-stu/ && yarn run build 
-rm -Rf /opt/nginxdocker/muitkid-web-app/mkstu
-cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-stu/dist /opt/nginxdocker/muitkid-web-app/mkstu
+cd muitkid-stu/  && eval ${yarnbash}
+cd ..
+cd muitkid-tea/  && eval ${yarnbash}
+cd ..
+cd muitkid-portal/  && eval ${yarnbash}
 
-cd ..
-cd muitkid-tea/ && yarn run build
-rm -Rf /opt/nginxdocker/muitkid-web-app/mktea
-cp -Rf /opt/nginxdocker/mulkids-cms-pro/muitkid-tea/dist /opt/nginxdocker/muitkid-web-app/mktea
 
